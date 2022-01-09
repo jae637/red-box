dbdc30177aabf5ff042c9a0b05b360967b5af2fc
Karl <jae637@naver.com>
2022-01-09 23:35:08 +0900

    update 작업중

---
+++ Assets/Scripts/EnermyAI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnermyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rigid;
+    public int nextMove;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        Think();
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        if (rigid.position.x > 7.9)
+            rigid.position = new Vector2(7.9f, rigid.position.y);
+        if (rigid.position.x < -7.9)
+            rigid.position = new Vector2(-7.9f, rigid.position.y);
+    }
+
+    void Think()
+    {
+        nextMove = Random.Range(-5, 5);
+        // if (rigid.velocity.x == 0)
+        //     Think();
+        Invoke("Think", 5);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            Debug.Log("hit");
+    }
+}
